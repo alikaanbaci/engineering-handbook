05611292d774334eb151a3697cf39f815f9a606a
Ali Kaan Bacı <alikaanbaci@gmail.com>
2025-09-20 19:26:41 +0300

    add base folder structure and example files #2

---
+++ docusaurus.config.js
@@ -0,0 +1,183 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import { themes as prismThemes, themes } from "prism-react-renderer";
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Enginering Notes",
+  tagline: "Contains general software engineering notes in a well designed and structured manner.",
+  favicon: "img/favicon.ico",
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: "https://your-docusaurus-site.example.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: false,
+        blog: false,
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      }),
+    ],
+  ],
+  markdown: {
+    mermaid: true,
+  },
+  themes: ["@docusaurus/theme-mermaid"],
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: "img/docusaurus-social-card.jpg",
+      prism: {
+        theme: themes.oneLight,
+        additionalLanguages: ["java", "mermaid"],
+      },
+      navbar: {
+        title: "Software Engineering",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            docsPluginId: "software-development",
+            label: "Software Development",
+            position: "left",
+          },
+          {
+            type: "doc",
+            docId: "intro",
+            docsPluginId: "distributed-systems",
+            label: "Distributed Systems",
+            position: "left",
+          },
+          {
+            type: "doc",
+            docId: "intro",
+            docsPluginId: "data-storage",
+            label: "Data Storage",
+            position: "left",
+          },
+          { to: "/about", label: "About", position: "right" },
+          {
+            href: "https://github.com/facebook/docusaurus",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Tutorial",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/docusaurus",
+              },
+              {
+                label: "Discord",
+                href: "https://discordapp.com/invite/docusaurus",
+              },
+              {
+                label: "X",
+                href: "https://x.com/docusaurus",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "/blog",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/facebook/docusaurus",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+    }),
+  plugins: [
+    [
+      "@docusaurus/plugin-content-docs",
+      {
+        id: "software-development",
+        path: "software-development",
+        routeBasePath: "software-development",
+        sidebarPath: require.resolve("./sidebars.js"),
+      },
+    ],
+    [
+      "@docusaurus/plugin-content-docs",
+      {
+        id: "distributed-systems",
+        path: "distributed-systems",
+        routeBasePath: "distributed-systems",
+        sidebarPath: require.resolve("./sidebars.js"),
+      },
+    ],
+    [
+      "@docusaurus/plugin-content-docs",
+      {
+        id: "data-storage",
+        path: "data-storage",
+        routeBasePath: "data-storage",
+        sidebarPath: require.resolve("./sidebars.js"),
+      },
+    ],
+  ],
+};
+
+export default config;
---
+++ software-development/spring/spring-parallel-tasks.md
@@ -0,0 +1,3 @@
+# Parallel Tasks In Spring Boot
+
+TBD - Contains how to implement parallel tasks in spring boot.
